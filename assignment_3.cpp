e7a1c827b9164ec4eb19d93f4b3397759344f4ed
hermionewarr <60819221+hermionewarr@users.noreply.github.com>
2020-02-19 17:37:36 +0000

    Add files via upload

--- assignment_3.cpp
+++ assignment_3.cpp
@@ -11,15 +11,16 @@
 #include<algorithm>
 #include<string.h>
 
+//comparing functions
+bool compare_by_course_name(std::string course_name1, std::string course_name2) {
+
+	return course_name1.substr(9) < course_name2.substr(9);
+}
 
 int main()
 {
-    const std::string degreeCode("PHYS");
-	int course_code{};
-    std::string course_name;
+    //main variables
     std::vector<std::string> course;
-	std::string user_input;
-	std::stringstream full_course_details;
 	std::vector<std::string> sorted_by_year;
 
     // Gather list of courses and their codes from user,
@@ -27,31 +28,31 @@ int main()
     bool not_finished(true);
     do
     {
+		//local variables
+		const std::string degreeCode("PHYS");
+		int course_code{};
+		std::string course_name;
+		std::string user_input;
+		std::stringstream full_course_details;
+
         std::cout << "Please enter a course code and course name (or x to finish): \n";//seems really slow file?
 		std::cin >> user_input;
-		int i{};
+
         if (user_input == "x") {
             not_finished = false;
         }
-        else if (user_input.empty()) {
-            std::cout << "Please enter a valid course: " << std::endl;
-            user_input.clear();//or erase?
-            std::cin >> user_input;
-        }
-
 		else {
 			std::stringstream(user_input) >> course_code;
-			std::getline(std::cin, course_name);
-			full_course_details << degreeCode << course_code << course_name;
+			std::getline(std::cin >> std::ws, course_name);
+			full_course_details << degreeCode << course_code << "  " << course_name;
 			course.push_back(full_course_details.str());
-			full_course_details.str("");
+			//full_course_details.str(""); //to clear
 		}
 
     } while (not_finished);
 
     // Print out full list of courses
     std::cout << "List of courses:" << std::endl;
-	//wants it done with intorators
 	std::vector <std::string> ::iterator vector_begin{ course.begin() };
 	std::vector <std::string>::iterator vector_end{course.end()};
 	std::vector <std::string>::iterator vector_iterator;
@@ -65,62 +66,49 @@ int main()
     std::cin >> year;
 	std::cout << "You entered year " << year << std::endl;
  
-	//sort by year
-	/*std::vector <std::string> ::iterator iterator_begin{ course.begin() };
+	//sort by year (not working but should
+	/*std::vector <std::string> ::iterator iiterator_begin{ course.begin() };
 	//std::vector<std::string> ::iterator iterator_year{ course.at(4) };
-	std::vector <std::string>::iterator iterator_end{ course.end() };
-	std::vector <std::string>::iterator iterator;
-	for (iterator = iterator_begin; iterator < iterator_end; ++iterator) {
-		if (iterator[4, 1] == year) {
-			std::cout << iterator[4,1] << std::endl;
-			sorted_by_year.push_back(*iterator);
-			std::cout << *iterator << std::endl;
+	std::vector <std::string>::iterator iiterator_end{ course.end() };
+	std::vector <std::string>::iterator iiterator;
+	for (iiterator = iiterator_begin; iiterator < iiterator_end - 1; ++iiterator) {
+		if (iiterator[4, 1] == year) {
+			std::cout << iiterator[4,1] << std::endl;
+			sorted_by_year.push_back(*iiterator);
+			std::cout << *iiterator << std::endl;
 		}
 	}*/
-
+	//working method to sort by year
 	for (size_t i{}; i < course.size(); ++i) {
 
 		if(course[i].substr(4,1) == year){
 				sorted_by_year.push_back(course[i]);
 		}
 	}
-	std::vector <std::string> ::iterator iterator_begin{ sorted_by_year.begin() };
-	std::vector <std::string>::iterator iterator_end{ sorted_by_year.end() };
-	std::vector <std::string>::iterator iterator;
-	for (iterator = iterator_begin; iterator < iterator_end; ++iterator)
-		std::cout << *iterator << std::endl;
-	//for (int i{}; i < sorted_by_year.size(); i++)
-		//std::cout << sorted_by_year[i] << std::endl;
+	//print out courses extracted by year with iterators
+	std::vector <std::string> ::iterator year_begin{ sorted_by_year.begin() };
+	std::vector <std::string>::iterator year_end{ sorted_by_year.end() };
+	std::vector <std::string>::iterator year_iterator;
+	for (year_iterator = year_begin; year_iterator < year_end; ++year_iterator)
+		std::cout << *year_iterator << std::endl;
 
     //sort courses by code or name
     std::string sort_option;
     std::cout << "Sort by code or name: ";
     std::cin >> sort_option;
+	std::cout << "Sorted by " << sort_option << ":" << std::endl;
 
-	//ok lot of stuff to fix here
-    if (sort_option == "code"){//theres a sort function google
-		/*std::vector <std::string>:: iterator begin {course.begin ()};
-		std::vector <std::string>::iterator end {course.end() };*/
-		//std::vector <std::string>::iterator vector_iterator;
-		//// Sort data in reverse order
-		//reverse(vector_begin, vector_end);
-		//std::cout << "Sorted data:" << std::endl;B
-		//std::vector <std::string>::iterator vector_iterator;
-		//for (vector_iterator = vector_begin; vector_iterator < vector_end; ++vector_iterator)
-		//	std::cout << *vector_iterator << std::endl;
-
-		/*sort(begin(course), end(course), substring_compare(4, 5));
-		std::cout << "Sorted by code: " << std::endl;
-		for (auto& line : course)
-			std::cout << line << std::endl;*/
-
+    if (sort_option == "code"){
+		std::sort(course.begin(), course.end());
+		for (std::vector<std::string>::iterator sorted_course_it = course.begin(); sorted_course_it != course.end(); ++sorted_course_it)
+			std::cout << ' ' << *sorted_course_it << std::endl;
     }
-
 	else if (sort_option == "name") {
-
+		std::sort(course.begin(), course.end(), compare_by_course_name);
+		for (std::vector<std::string>::iterator sorted_course_it = course.begin(); sorted_course_it != course.end(); ++sorted_course_it)
+			std::cout << ' ' << *sorted_course_it << std::endl;
 	}
     else {
-		//if your keeping this figure it out
         std::cout << "Please say whether you would like to sort by code or name: (or another charracter to quit)" << std::endl;
         std::cin.clear();
         std::cin.ignore();
@@ -128,7 +116,4 @@ int main()
     }
 
     return 0;
-
 }
-
-
